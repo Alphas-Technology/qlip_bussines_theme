49c3a68de5c29e204b1a249ba26bfa7b203bccc4
Leonardo Alviarez <leonardo.alviarez@alphas.technology>
2020-07-26 17:01:11 +0000

    Activated css for desk.html

--- qlip_bussines_theme/hooks.py
+++ qlip_bussines_theme/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/qlip_bussines_theme/css/qlip_bussines_theme.css"
+app_include_css = "/assets/qlip_bussines_theme/css/qlip_bussines_theme.css"
 # app_include_js = "/assets/qlip_bussines_theme/js/qlip_bussines_theme.js"
 
 # include js, css files in header of web template
