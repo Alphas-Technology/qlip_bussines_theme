5719e828157d0d13bc0fd52b7a808c7f3f2de34b
Leonardo Alviarez <leonardo.alviarez@alphas.technology>
2020-08-04 06:29:56 +0000

    Added styles and behavior for Data Control (input text)

--- qlip_bussines_theme/hooks.py
+++ qlip_bussines_theme/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/qlip_bussines_theme/css/qlip_bussines_theme.css"
-# app_include_js = "/assets/qlip_bussines_theme/js/qlip_bussines_theme.js"
+app_include_js = "/assets/qlip_bussines_theme/js/qlip_bussines_theme.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/qlip_bussines_theme/css/qlip_bussines_theme.css"
---
+++ qlip_bussines_theme/public/js/qlip_bussines_theme.js
@@ -0,0 +1,65 @@
+// Control Data (textbox)
+frappe.provide('frappe.phone_call');
+
+frappe.ui.form.ControlData = frappe.ui.form.ControlData.extend({
+    make_input: function() {
+		if(this.$input) return;
+
+		this.$input = $("<"+ this.html_element +">")
+			.attr("type", this.input_type)
+			.attr("autocomplete", "off")
+			.addClass("input-with-feedback form-control floating-input")
+			.prependTo(this.input_area);
+
+		if (in_list(['Data', 'Link', 'Dynamic Link', 'Password', 'Select', 'Read Only', 'Attach', 'Attach Image'],
+			this.df.fieldtype)) {
+			this.$input.attr("maxlength", this.df.length || 140);
+		}
+
+		this.set_input_attributes();
+		this.input = this.$input.get(0);
+		this.has_input = true;
+		this.bind_change_event();
+		this.bind_focusout();
+		this.setup_autoname_check();
+		if (this.df.options == 'Phone') {
+			this.setup_phone();
+		}
+		// somehow this event does not bubble up to document
+		// after v7, if you can debug, remove this
+    },
+    
+    make_wrapper: function() {
+		if(this.only_input) {
+			this.$wrapper = $('<div class="form-group frappe-control">').appendTo(this.parent);
+		} else {
+			this.$wrapper = $('<div class="frappe-control">\
+				<div class="form-group">\
+					<div class="control-input-wrapper">\
+                        <div class="control-input floating-label ">\
+                        <label class="control-label" style="padding-right: 0px;"></label>\
+                        </div>\
+						<div class="control-value like-disabled-input" style="display: none;"></div>\
+						<p class="help-box small text-muted hidden-xs"></p>\
+					</div>\
+				</div>\
+			</div>').appendTo(this.parent);
+		}
+	},
+    
+    set_input_attributes: function() {
+		this.$input
+			.attr("data-fieldtype", this.df.fieldtype)
+			.attr("data-fieldname", this.df.fieldname)
+			.attr("placeholder", this.df.placeholder || " ");
+		if(this.doctype) {
+			this.$input.attr("data-doctype", this.doctype);
+		}
+		if(this.df.input_css) {
+			this.$input.css(this.df.input_css);
+		}
+		if(this.df.input_class) {
+			this.$input.addClass(this.df.input_class);
+		}
+	}
+})
