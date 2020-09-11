cbd5649bc237862da5c818633b8d6afb9dd88c43
Leonardo Alviarez <leonardo.alviarez@alphas.technology>
2020-09-11 07:25:02 +0000

    [Add] Logic for form control Float

--- qlip_bussines_theme/public/js/qlip_bussines_theme.js
+++ qlip_bussines_theme/public/js/qlip_bussines_theme.js
@@ -224,7 +224,34 @@ frappe.ui.form.ControlInt = frappe.ui.form.ControlData.extend({
 // End Control Int
 
 // Control Float
-frappe.ui.form.ControlFloat = frappe.ui.form.ControlInt.extend({})
+frappe.ui.form.ControlFloat = frappe.ui.form.ControlInt.extend({
+	parse: function(value) {
+		value = this.eval_expression(value);
+		return isNaN(parseFloat(value)) ? null : flt(value, this.get_precision());
+	},
+
+	format_for_input: function(value) {
+		var number_format;
+		if (this.df.fieldtype==="Float" && this.df.options && this.df.options.trim()) {
+			number_format = this.get_number_format();
+		}
+		var formatted_value = format_number(value, number_format, this.get_precision());
+		return isNaN(parseFloat(value)) ? "" : formatted_value;
+	},
+
+	get_number_format: function() {
+		var currency = frappe.meta.get_field_currency(this.df, this.get_doc());
+		return get_number_format(currency);
+	},
+
+	get_precision: function() {
+		// round based on field precision or float precision, else don't round
+		return this.df.precision || cint(frappe.boot.sysdefaults.float_precision, null);
+	}
+});
+
+frappe.ui.form.ControlPercent = frappe.ui.form.ControlFloat;
+
 // End Control Float
 
 // Control Currency
